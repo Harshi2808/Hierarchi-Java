46e08ae5928425d8a69fc9b3f32f7beeeaaab452
Harshi2808 <kalashri20@gmail.com>
2022-11-21 13:31:54 +0530

    Sample Data

---
+++ src/datatypes/program/EmployeeDetails.java
@@ -0,0 +1,24 @@
+package datatypes.program;
+
+public class EmployeeDetails {
+public static void main(String[] args) {
+	byte age=25;
+	System.out.println("employee age is "+age);
+	short year=2022;
+	System.out.println("employee joining year is "+year);
+	int pan=123456789;
+	System.out.println("employee pan is "+pan);
+	long aadharnum=1234567891023456l;
+	System.out.println("employee aadharnumber is "+aadharnum);
+	float salary=48500.50f;
+	System.out.println("employee salary is "+salary);
+	double marketvalue=123456.5566;
+	System.out.println("employee market value is "+marketvalue);
+	char gender='F';
+	System.out.println("employee gender is "+gender);
+	String name="Kala";
+	System.out.println("employee name is "+name);
+	boolean citizen=true;
+	System.out.println("employee citizenship is Indian "+true);
+	}
+}
---
+++ src/org/hcl/test/TraineeDetails.java
@@ -0,0 +1,15 @@
+package org.hcl.test;
+
+public class TraineeDetails {
+public void batchOne() {
+	System.out.println("training started on Sep");
+}
+public void batchTwo() {
+	System.out.println("training started on Oct");
+}
+public static void main(String[] args) {
+	TraineeDetails abc=new TraineeDetails();
+	abc.batchOne();
+	abc.batchTwo();
+}
+}
